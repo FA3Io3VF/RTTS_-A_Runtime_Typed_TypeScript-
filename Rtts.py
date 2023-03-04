ca683c52e9937f9fea94eb7c8ab74ae140a4dd8c
FA3Io3VF <126454909+FA3Io3VF@users.noreply.github.com>
2023-03-04 17:24:44 +0100

    Create Rtts.py

---
+++ Rtts.py
@@ -0,0 +1,90 @@
+import os
+import subprocess
+import antlr4
+from JavaScriptLexer import JavaScriptLexer
+from JavaScriptParser import JavaScriptParser
+from JavaScriptTypeChecker import JavaScriptTypeChecker
+
+class PrintVisitor(JavaScriptParser.JavaScriptParserVisitor):
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.result = ""
+
+    def visitProgram(self, ctx:JavaScriptParser.ProgramContext):
+        for child in ctx.children:
+            self.result += self.visit(child)
+
+    def visitStatement(self, ctx:JavaScriptParser.StatementContext):
+        result = self.visit(ctx.getChild(0))
+        if result is not None:
+            self.result += result + "\n"
+        return ""
+
+    def visitFunctionDeclaration(self, ctx:JavaScriptParser.FunctionDeclarationContext):
+        result = self.visit(ctx.getChild(0)) + "("
+        params = []
+        for param in ctx.formalParameterList().formalParameter():
+            params.append(self.visit(param))
+        result += ", ".join(params) + ") {\n"
+        result += self.visit(ctx.functionBody())
+        result += "}\n"
+        return result
+
+    def visitVariableDeclaration(self, ctx:JavaScriptParser.VariableDeclarationContext):
+        result = self.visit(ctx.getChild(0)) + " "
+        declarations = []
+        for declaration in ctx.variableDeclarationList().variableDeclaration():
+            name = self.visit(declaration.getChild(0))
+            if declaration.getChildCount() == 1:
+                declarations.append(name)
+            else:
+                initializer = self.visit(declaration.getChild(2))
+                declarations.append(f"{name} = {initializer}")
+        result += ", ".join(declarations) + ";"
+        return result
+
+    def visitIdentifierExpression(self, ctx:JavaScriptParser.IdentifierExpressionContext):
+        return ctx.getText()
+
+    def visitLiteralExpression(self, ctx:JavaScriptParser.LiteralExpressionContext):
+        return ctx.getText()
+
+    def visitParenthesizedExpression(self, ctx:JavaScriptParser.ParenthesizedExpressionContext):
+        return f"({self.visit(ctx.expression())})"
+
+    def visitAdditiveExpression(self, ctx:JavaScriptParser.AdditiveExpressionContext):
+        left = self.visit(ctx.getChild(0))
+        right = self.visit(ctx.getChild(2))
+        operator = ctx.getChild(1).getText()
+        return f"{left} {operator} {right}"
+
+#Compila il file TypeScript in JavaScript
+ts_file = "path/to/typescript/file.ts"
+js_file = "path/to/javascript/file.js"
+subprocess.run(["npx", "tsc", "--outFile", js_file, ts_file], check=True)
+
+#Creare un'istanza della classe JavaScriptTypeChecker
+type_checker = JavaScriptTypeChecker()
+
+#Analizza il codice JavaScript utilizzando la grammatica ANTLR
+input_stream = antlr4.FileStream(js_file)
+lexer = JavaScriptLexer(input_stream)
+stream = antlr4.CommonTokenStream(lexer)
+parser = JavaScriptParser(stream)
+tree = parser.program()
+
+#Visita l'albero di sintassi generato con la classe JavaScriptTypeChecker
+type_checker.visit(tree)
+
+#Definisce il percorso del file di output
+output_file = "path/to/javascript/file_with_type_checks.js"
+
+#Utilizza PrintVisitor per generare il nuovo file JS con i controlli di tipo aggiunti
+visitor = PrintVisitor(output_file)
+visitor.visit(tree)
+
+#Scrive il risultato nel file di output
+with open(output_file, "w") as f:
+    f.write(visitor.result)
+
+print(f"File generato: {output_file}")
